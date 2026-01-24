d1ef2aed09ed99ffe42fe55d8b4f1bc783094bc3
unknown <lefta.chandra@gmail.com>
2026-01-24 22:03:56 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# slims-barcode-eksemplar
+
+Plugin barcode eksemplar Arie Nugraha, plugin oleh Imanuel Chandra Lefta
+
+![Barcode Eksemplar SLiMS](screenshot.png)
---
+++ barcode.plugin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: Barcode
+ * Plugin URI: -
+ * Description: Plugin barcode eksemplar oleh Arie Nugraha
+ * Version: 1.0.0
+ * Author: Arie Nugraha, plugin oleh Imanuel Chandra Lefta, ST
+ * Author URI: -
+ */
+use SLiMS\Plugins;
+
+$plugin = Plugins::getInstance();
+
+Plugins::getInstance()->registerAutoload(__DIR__);
+
+$path =  __DIR__ . '/pages/item_barcode_generator.php';
+ // Make default group menu
+Plugins::group('Kelengkapan Bahan Pustaka', function() use($path) {
+            // Scan all file inside module directory as menu
+Plugins::menu('bibliography', 'Barcode', $path);
+});
